8da83e555034ce7a4962fcfb612d7dcba8fafdef
denis <hohol386@ukr.net>
2021-11-09 12:15:26 +0200

    update3

---
+++ src/app/home_page_components/serials/serials.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SerialsComponent } from './serials.component';
+
+describe('SerialsComponent', () => {
+  let component: SerialsComponent;
+  let fixture: ComponentFixture<SerialsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SerialsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SerialsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
