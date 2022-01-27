5ca58e49481915081bb3231f69be2899bbd4ecd9
denis <hohol386@ukr.net>
2022-01-27 04:37:29 +0200

    update routing serials

---
+++ src/app/home_page_components/serial-info/serialInfoModel.ts
@@ -0,0 +1,16 @@
+export class SerialInfoModel{
+  id:number
+  description:string
+  image_url:string
+  original_name:string
+  premiere:Date
+  rating:string
+  start_of_filming:string
+  status:string
+  youtube_trailer_url:string
+  audioTracks:Array<any>
+  actors:Array<any>
+
+
+
+}
--- src/app/shared_services/httpService/auth.service.ts
+++ src/app/shared_services/httpService/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient,HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {SignupRequestPayload} from "../../base_components/auth/signup/signup-request.payload";
+import {SerialInfoModel} from "../../home_page_components/serial-info/serialInfoModel";
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +16,11 @@ export class AuthService {
   signup(signupRequestPayload:SignupRequestPayload): Observable<any>{
       return  this.httpClient.post(this.serverAddress + '/auth/signup',
         signupRequestPayload,{responseType:"text"})
+  }
+
+  getAllSerials(id:any) : Observable<any>{
+    // let qwe: any = {'id': id};
+    let param = new HttpParams().set('id', id);
+    return this.httpClient.post(this.serverAddress + '/auth/getPostById',param)
   }
 }
